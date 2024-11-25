69f875d4abe971bd004e52586fc1005607e26692
Juhani Astikainen <juhani.astikainen@gmail.com>
2024-11-25 20:00:09 +0200

    fix: prefer https

--- scripts/foli-api.js
+++ scripts/foli-api.js
@@ -7,6 +7,6 @@ async function getAllVehicleInformation() {
 
 async function getStops() {
   console.debug("[getStops] Fetching stops from API (sort of expensive operation)");
-  return fetch(`http://data.foli.fi/gtfs/v0/${hardCodedLatestDataPackage}/stops`)
+  return fetch(`https://data.foli.fi/gtfs/v0/${hardCodedLatestDataPackage}/stops`)
     .then(response => response.json())
 }
